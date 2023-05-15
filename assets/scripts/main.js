916f24cc2aa840c54d3f1d0e843329c8e02f370d
Leon Tan <leontann29@gmail.com>
2023-05-14 18:32:01 -0700

    finished Part 1 Expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,9 +21,16 @@ function init() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 function getRecipesFromStorage() {
-  // A9. TODO - Complete the functionality as described in this function
+  // A9. Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+    let text = localStorage.getItem('recipes');
+    if (!text) {
+	text = '[]';
+    }
+    // console.log(text);
+    let recipes = JSON.parse(text);
+    return recipes;
 }
 
 /**
@@ -34,11 +41,19 @@ function getRecipesFromStorage() {
  * @param {Array<Object>} recipes An array of recipes
  */
 function addRecipesToDocument(recipes) {
-  // A10. TODO - Get a reference to the <main> element
-  // A11. TODO - Loop through each of the recipes in the passed in array,
+  // A10. Get a reference to the <main> element
+    let main = document.querySelector("main");
+
+  // A11. Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+    for (let i = 0; i < recipes.length; i++) {
+	let card = document.createElement("recipe-card");
+	card.data = recipes[i];
+	main.appendChild(card);
+	// console.log(`created ${i}`);
+    }
 }
 
 /**
